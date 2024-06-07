7695fc74b9be821d14f30f9a117b983a6f4a59fa
marissatol <153950982+marissatol@users.noreply.github.com>
2024-06-07 15:57:07 +0200

    Update PhiCalc.py
    updated comments

--- PhiCalc.py
+++ PhiCalc.py
@@ -7,68 +7,75 @@ Created on Fri May 24 13:52:55 2024
 import numpy as np
 from scipy import optimize
 
-# (radian) frequency: ?
+# (Radian) frequency: ?
 sigma = 1
 
-# wavelength: ?
+# Wavelength: ?
 lamda = 1
 
-# wavenumber: 2pi/wavelength lambda
+# Wavenumber: 2pi/wavelength lambda
 kappa = 2 * np.pi / lamda
 
-# wave amplitude: ?
+# Wave amplitude: ?
 a = 1
 
-# water pressure: 1025 kg/m^2
+# Water pressure: 1025 kg/m^2
 rho = 1025
 
-# inertia coefficient: 1+C_a ~ 8.66
+# Inertia coefficient: 1+C_a ~ 8.66
 C_m = 8.66
 
-# drag coefficient: 1.17
+# Drag coefficient: 1.17
 C_D = 1.17
 
-# cylinder diameter: ?
+# Cylinder diameter: ?
 D = 5
 
 # Horizontal bottom: < 50
 H = 50
 
 
+# Function that calculates phi
 def calc_phi(z, x, t):
     phi = sigma / kappa * a * np.cosh(kappa * (z+H)) / \
         np.sinh(kappa*H) * np.sin((kappa * x)+(sigma * t))
     return phi
 
 
+# Function that calculates the differential of phi
 def diff_phi(z, x, t):
     d_phi = sigma * kappa * a * np.cosh(kappa * (z + H)) / \
         np.sinh(kappa * H) * np.cos((kappa * x) + (sigma * t))
     return d_phi
 
 
+# Wave force function/morrison equation
 def wave_force(z, t):
     F = (np.pi / 4 * rho * C_m * D ** 2 * diff_u(z, t)) + \
         1/2 * rho * C_D * D * u(z, t) * np.abs(u(z, t))
     return F
 
 
+# Function that calculates u.
 def u(z, t):
     u = sigma * a * np.cosh(kappa * (z + H)) / \
         np.sinh(kappa * H) * np.cos(sigma * t)
     return u
 
 
+# function for the differential of u in z?
 def diff_u(z, t):
     diff_u = - sigma ** 2 * a * np.cosh(kappa * (z + H)) / \
         np.sinh(kappa*H) * np.sin(sigma * t)
     return diff_u
 
 
+# Function of which the zeros have to be found.
 def f(x):
     return np.cosh(x)*np.cos(x)+1
 
 
+# Apply bisection method to find the zeros of a function.
 def find_roots(func, n, a, b):
     zeros = [[[], [], []]for i in range(n)]
     stepsize = (b-a)/n
@@ -86,17 +93,18 @@ def find_roots(func, n, a, b):
     return zeros
 
 
-def q(x):
+def test_q(x):
     return np.sin(x)
 
 
 print(find_roots(f, 100, 0, 100))
 
 
-def func(x):
+def test_func(x):
     return x ** 5
 
 
+# Application of simpson's rule to find the integral of a function.
 def simps_rule(func, a, b, n):
     h = (b-a)/n
     step = a
@@ -108,4 +116,4 @@ def simps_rule(func, a, b, n):
     return res
 
 # test, answer should be 12
-# print(simps_rule(func, 0, 2, 1))
+# print(simps_rule(test_func, 0, 2, 1))
